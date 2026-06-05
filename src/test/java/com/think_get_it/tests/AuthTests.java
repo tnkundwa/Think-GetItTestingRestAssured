930a699cfc87a86b6265d8c6cd320c3c44a2aa39
Toussaint NKUNDWA <toussaintnkundwa@gmail.com>
2026-06-05 14:21:27 +0200

    Worked on some reviews about hard coded values in the test classes

--- src/test/java/com/think_get_it/tests/AuthTests.java
+++ src/test/java/com/think_get_it/tests/AuthTests.java
@@ -1,35 +1,25 @@
 package com.think_get_it.tests;
 
 import com.think_get_it.api.applicationApi.AuthApi;
-import com.think_get_it.pojo.request.LoginUserReq;
-import com.think_get_it.pojo.request.RegisterUser;
 import com.think_get_it.pojo.response.ResponsePojo;
 import com.think_get_it.pojo.response.UserDataPojo;
-import com.think_get_it.utils.ConfigLoader;
 import org.testng.annotations.Test;
 
+import static com.think_get_it.api.applicationApi.RestResources.newUser;
+import static com.think_get_it.api.applicationApi.RestResources.userLogin;
 import static org.testng.Assert.assertNotNull;
 
 public class AuthTests {
 
     @Test
     public void registerUser() {
-        RegisterUser request = new RegisterUser();
-        request.setEmail("jo12@x.com");
-        request.setPassword("MyPass@123");
-        request.setFirstName("John");
-        request.setLastName("Doe");
-        request.setPhone("+250788123456");
-        ResponsePojo<UserDataPojo> res = AuthApi.registerUser(request);
+        ResponsePojo<UserDataPojo> res = AuthApi.registerUser(newUser());
         assertNotNull(res, "The response should not be null");
     }
 
     @Test
     public void loginUser() {
-        LoginUserReq request = new LoginUserReq();
-        request.setEmail(ConfigLoader.getInstance().getUserEmail());
-        request.setPassword(ConfigLoader.getInstance().getPassword());
-        ResponsePojo<UserDataPojo> res = AuthApi.login(request);
+        ResponsePojo<UserDataPojo> res = AuthApi.login(userLogin());
         assertNotNull(res, "The response should not be null");
     }
 }
--- src/test/java/com/think_get_it/tests/UserTests.java
+++ src/test/java/com/think_get_it/tests/UserTests.java
@@ -5,9 +5,8 @@ import com.think_get_it.pojo.response.ResponsePojo;
 import com.think_get_it.pojo.response.User;
 import org.testng.annotations.Test;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import static com.think_get_it.api.applicationApi.RestResources.userChangePassword;
+import static com.think_get_it.api.applicationApi.RestResources.userUpdate;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.testng.Assert.assertEquals;
@@ -24,11 +23,7 @@ public class UserTests {
 
     @Test
     public void updateUserProfileTest() {
-        Map<String, String> request = new HashMap<>();
-        request.put("firstName", "T");
-        request.put("lastName", "N");
-        request.put("phone", "+250788123456");
-        ResponsePojo<User> res = UserApi.updateUser(request);
+        ResponsePojo<User> res = UserApi.updateUser(userUpdate());
         assertNotNull(res, "The response should not be null");
         assertThat(res.getSuccess(), is(true));
         assertEquals(res.getMessage(), "Profile updated");
@@ -36,10 +31,7 @@ public class UserTests {
 
     @Test
     public void changePasswordTest() {
-        Map<String, String> request = new HashMap<>();
-        request.put("currentPassword", "");
-        request.put("newPassword", "");
-        ResponsePojo<User> res = UserApi.changeUserPassword(request);
+        ResponsePojo<User> res = UserApi.changeUserPassword(userChangePassword());
         assertNotNull(res, "The response should not be null");
         assertThat(res.getSuccess(), is(true));
         assertEquals(res.getMessage(), "Password changed");
