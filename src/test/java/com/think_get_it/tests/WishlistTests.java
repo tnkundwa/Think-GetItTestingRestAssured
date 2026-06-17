b50c12e0485d6bd719563d379c787e0e3587a389
Toussaint NKUNDWA <toussaintnkundwa@gmail.com>
2026-06-17 11:33:20 +0200

    Fully implemented the wishlist api

--- src/test/java/com/think_get_it/tests/WishlistTests.java
+++ src/test/java/com/think_get_it/tests/WishlistTests.java
@@ -5,26 +5,49 @@ import org.testng.annotations.Test;
 
 import static com.think_get_it.api.applicationApi.RestResources.getProductId;
 import static com.think_get_it.api.applicationApi.WishlistApi.*;
-import static org.testng.Assert.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.testng.Assert.*;
 
 public class WishlistTests {
 
     @Test
     public void getUserWishlistTest() {
         Response res = getUsersWishlist();
-        assertEquals(res.statusCode(), 200);
+        boolean success = res.jsonPath().getBoolean("success");
+        String message = res.jsonPath().getString("message");
+        java.util.Map<Object, Object> data = res.jsonPath().getMap("data");
+        assertTrue(success);
+        assertEquals(message, "Success");
+        assertFalse(data.isEmpty());
     }
 
     @Test
     public void addProductToWishlistTest() {
         Response res = addProductToWishlist(getProductId("Ubrtiuob"));
-        assertEquals(res.statusCode(), 201);
+        String name = res.jsonPath().getString("data[0].name");
+        String slug = res.jsonPath().getString("data[0].slug");
+        String description = res.jsonPath().getString("data[0].description");
+        boolean success = res.jsonPath().getBoolean("success");
+        String message = res.jsonPath().getString("message");
+        java.util.Map<Object, Object> data = res.jsonPath().getMap("data");
+        assertTrue(success);
+        assertEquals(message, "Added to wishlist");
+        assertFalse(data.isEmpty());
+        assertThat(name, is(not(emptyOrNullString())));
+        assertThat(slug, is(not(emptyOrNullString())));
+        assertThat(description, is(not(emptyOrNullString())));
     }
 
     @Test
     public void removeProductFromWishlistTest() {
         Response res = removeProductFromWishlist(getProductId("Ubrtiuob"));
-        assertEquals(res.statusCode(), 200);
+        boolean success = res.jsonPath().getBoolean("success");
+        String message = res.jsonPath().getString("message");
+        java.util.Map<Object, Object> data = res.jsonPath().getMap("Removed from wishlist");
+        assertTrue(success);
+        assertEquals(message, "Added to wishlist");
+        assertTrue(data.isEmpty());
     }
 
 }
